af4ec3e19dea0a8b6da119787720af10c7db2dad
Dewayne Richardson <dewrich@gmail.com>
2019-03-04 15:03:44 -0700

    completed the config section

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,24 +1,18 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
-    public String getWelcomeMessage() {
-        return welcomeMessage;
-    }
-
-    public void setWelcomeMessage(String welcomeMessage) {
-        this.welcomeMessage = welcomeMessage;
-    }
-
-    @Value("${welcome.message}")
     private String welcomeMessage;
 
-    public WelcomeController(String welcomeMessage)
+
+    public WelcomeController( @Value("${welcome.message}") String welcomeMessage)
     {
         this.welcomeMessage = welcomeMessage;
     }
